2390033a04115e22489ffffbe319b365fd01c169
vagrant <avedj94@hotmail.com>
2020-11-03 14:00:53 +0000

    type sentence with delay using settimeout

---
+++ typewriter.js
@@ -0,0 +1,9 @@
+const sentence = "hello there from lighthouse labs";
+let count = 0;
+
+for (const char of sentence) {
+  setTimeout(() => {
+      process.stdout.write(char);
+  }, count * 50);
+  count++;
+}  
